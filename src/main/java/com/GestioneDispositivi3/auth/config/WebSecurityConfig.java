81518f55a70677822938a63ffac5fd4150531d7d
sabrizlt <sabrizaltini6@gmail.com>
2023-06-12 11:04:09 +0200

    ok

--- src/main/java/com/GestioneDispositivi3/auth/config/WebSecurityConfig.java
+++ src/main/java/com/GestioneDispositivi3/auth/config/WebSecurityConfig.java
@@ -28,6 +28,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private AuthEntryPointJwt unauthorizedHandler;
 	
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.cors().and()
